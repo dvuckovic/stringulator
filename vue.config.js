2094f9e955e778a4d1c38115ff534e4a719613c8
Dusan Vuckovic <dusan@dvuckovic.com>
2021-02-22 13:06:24 +0000

    Improved page title.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,17 @@
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
+const project = require('./package.json');
+const lodash = require('lodash');
+
 module.exports = {
     publicPath: '',
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: `${lodash.upperFirst(project.name)} - ${project.description}`,
+        },
+    },
     chainWebpack: config => {
         config.performance
             .maxEntrypointSize(245 * 1024);
