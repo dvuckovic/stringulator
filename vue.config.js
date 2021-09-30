91739890fadd680acbca30dd54a1f1a331f9f124
Dusan Vuckovic <dusan@dvuckovic.com>
2021-09-30 17:57:11 +0100

    Bumped up dependencies.

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     chainWebpack: config => {
         config.performance
-            .maxEntrypointSize(342 * 1024)
+            .maxEntrypointSize(355 * 1024)
             .maxAssetSize(680 * 1024);
     },
 };
